d47a3662c06ca4f4e311792ec404ce4ea06ad6ec
gwhitney15 <gwhitney115@gmail.com>
2022-08-30 03:26:13 -0500

    added prompt plus classes

--- index.js
+++ index.js
@@ -3,3 +3,25 @@ const Employee = require("./lib/Employee");
 const Engineer = require("./lib/Engineer");
 const Manager = require("./lib/Manager");
 const Intern = require("./lib/Intern");
+
+createTeam()
+function createTeam() {
+    inquirer
+        .prompt([
+            {
+                type: `input`,
+                name: `name`,
+                message: `Please enter the Team Manager's name.`,
+            },
+            {
+                type: `input`,
+                name: `id`,
+                message: `Please enter the Team Manager's ID.`,
+            },
+            {
+                type: `input`,
+                name: `email`,
+                message: `Please enter the Team Manager's email.`,
+            },
+        ])
+}
